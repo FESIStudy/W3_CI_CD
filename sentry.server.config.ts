ccbddb206e79861f21d03210ecb6610f408d2cda
thgee <thgeee@naver.com>
2025-02-06 01:43:02 +0900

    chore: CI/CD 구축 및 sentry 연동

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e18d31c575296e230b577098602b614e@o4508767389876224.ingest.us.sentry.io/4508767393677312",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
